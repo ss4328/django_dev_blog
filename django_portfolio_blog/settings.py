299b1e5e98dcf827b86783ed7121647ba78d950f
Shiv <ss4328@drexel.edu>
2021-02-14 20:00:49 -0500

    visual fixes

--- django_portfolio_blog/settings.py
+++ django_portfolio_blog/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
