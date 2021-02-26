5ef77d8955c1827fc58059b6034d178e39268c57
Shiv <ss4328@drexel.edu>
2021-02-25 21:25:58 -0500

    pagination & style fixes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('portfolio/',  TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.views import generic
 from .models import Post
 
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 4
+
 
 class PostDetail(generic.DetailView):
     model = Post
